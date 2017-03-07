dde5f855b9113172d209c933fe06f6f2b7186227
prashant <gaikarprashant63@gmail.com>
2017-03-07 19:29:27 +0530

    Library management

---
+++ src/main/java/configuration/OnlibWebAppInitialiser.java
@@ -0,0 +1,25 @@
+package configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class OnlibWebAppInitialiser extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
